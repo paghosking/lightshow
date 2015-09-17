081917c6df8d8b6de350678ec1cbac24a61dc8e6
Peter Hosking <peter.hosking@auckland.ac.nz>
2015-09-17 13:31:28 +1200

    two insruments working

--- python_scripts_for_performance_instruments/threading_test.py
+++ python_scripts_for_performance_instruments/threading_test.py
@@ -30,14 +30,14 @@ def instrument(instrument, midi_port):
 
     if instrument == 'keyboard':
 
-        # only valid id's should be processed
-        valid_ids = [144, 128]
+        # only valid key types and key ids
+        valid_key_types = [144, 128]
 
         while stopQ.empty():
             while midi_in.poll():
                 midi_event = midi_in.read(1)
                 # logging.debug(midi_event)
-                if midi_event[0][0][0] in valid_ids:
+                if midi_event[0][0][0] in valid_key_types:
                     key_id = midi_event[0][0][1]
                     if midi_event[0][0][2]:
                         vel = midi_event[0][0][2]
@@ -48,11 +48,20 @@ def instrument(instrument, midi_port):
 
     elif instrument == 'drums':
 
+        # only valid key types an key ids
+        valid_key_ids = [31, 46, 85, 47, 43, 33, 49, 51, 48, 42]
+
         while stopQ.empty():
             while midi_in.poll():
                 midi_event = midi_in.read(1)
-                # if midi_event[0][0][0] != 248:
-                logging.debug(midi_event)
+                # logging.debug(midi_event)
+                if midi_event[0][0][1] in valid_key_ids:
+                    key_id = midi_event[0][0][1]
+                    if midi_event[0][0][2]:
+                        vel = midi_event[0][0][2]
+                        drumsQ.put([key_id, vel])
+                    else:
+                        drumsQ.put([key_id, 0])
 
     else:
         while midi_in.poll():
@@ -63,7 +72,6 @@ def instrument(instrument, midi_port):
 
 
 def render_color(color, channel):
-    mydmx.setChannel(1, 255)
     mydmx.setChannel(channel, int(color[0] * 255))
     mydmx.setChannel(channel + 1, int(color[1] * 255))
     mydmx.setChannel(channel + 2, int(color[2] * 255))
@@ -78,40 +86,59 @@ def dmx():
     # initialize color class instances
     keyboard_color = lib.MidiColor(12)
     keyboard_color.change_mode(5)
+    drums_color = lib.MidiColor(12)
+    drums_color.change_mode(5)
+
+    # some dmx channels need to be set at a constant value
+    mydmx.setChannel(1, 255)
+    mydmx.setChannel(24, 255)
 
     while True:
         fps = clock.tick(60) # frame limit
         while not (keyboardQ.empty() and drumsQ.empty()):
             if not keyboardQ.empty():
-                midi_data =  keyboardQ.get()
+                midi_data = keyboardQ.get()
+                print midi_data
                 midi_data[0] %= 12
                 if midi_data[1]:
                     keyboard_color.add_color(midi_data[0], midi_data[1], VEL_MAX)
                 else:
                     keyboard_color.rem_color(midi_data[0])
             if not drumsQ.empty():
-                print drumsQ.get()
+                midi_data = drumsQ.get()
+                print midi_data
+                midi_data[0] %= 12
+                if midi_data[1]:
+                    drums_color.add_color(midi_data[0], midi_data[1], VEL_MAX)
+                else:
+                    drums_color.rem_color(midi_data[0])
+        # update color classes
         keyboard_color.update()
-        # print keyboard_color.output_color
+        drums_color.update()
+        # set dmx channels
         render_color(keyboard_color.output_color.rgb, 2)
+        render_color(drums_color.output_color.rgb, 20)
+        # render all dmx channels
         mydmx.render()
 
 
 # mydmx = pysimpledmx.DMXConnection('/dev/cu.usbserial-6AYP9O1D') # mac dmx com port
-# mydmx = pysimpledmx.DMXConnection(5)  # windows dmx com port
-mydmx = pysimpledmx.DMXConnection('/dev/ttyUSB0')  # linux com port
+mydmx = pysimpledmx.DMXConnection(6)  # windows dmx com port
+# mydmx = pysimpledmx.DMXConnection('/dev/ttyUSB0')  # linux com port
 
 # pygame midi initialization
 pygame.init()
 pygame.midi.init()
+
+# print pygame.midi.get_default_input_id()
 
 # set up queues
 stopQ = Queue.Queue()
 keyboardQ = Queue.Queue()
 drumsQ  = Queue.Queue()
 
-keyboard_thread = threading.Thread(name='keybaord', target=instrument, args=('keyboard', 3))
-drums_thread = threading.Thread(name='drums', target=instrument, args=('drums', 5))
+keyboard_thread = threading.Thread(name='keybaord', target=instrument, args=('keyboard', 1))
+drums_thread = threading.Thread(name='drums', target=instrument, args=('drums', 2))
 
 keyboard_thread.start()
 drums_thread.start()
